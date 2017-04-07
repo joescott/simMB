e0976484d0ae2f47e9372edb73960e2765fd9c4d
Jorge Courett <jorge.courett@gmail.com>
2017-04-06 22:33:53 -0300

    cbuff

--- src/test/test_cbuffer.c
+++ src/test/test_cbuffer.c
@@ -11,6 +11,31 @@
 
 int tests_run = 0;
 
+static _inc_readcbuffer(CBUFF *cb)
+{
+    printf("INC R\n");
+    return inc_readcbuffer(cb);
+}
+
+static _dec_readcbuffer(CBUFF *cb)
+{
+    printf("DEC R\n");
+    return dec_readcbuffer(cb);
+}
+
+static _inc_writecbuffer(CBUFF *cb)
+{
+    printf("INC W\n");
+    return inc_writecbuffer(cb);
+}
+
+static void print_cb(CBUFF *cb)
+{
+    printf("------------------------\n");
+    printf("R: %d <- %s\n",cb->read-cb->pool,  *(char**)cb->read);
+    printf("W: %d <- %s\n",cb->write-cb->pool, *(char**)cb->write);
+}
+
 int test_init_cbuffer()
 {
     CBUFF *cb = init_cbuffer(10);
@@ -38,87 +63,107 @@ int test_increment_cbuffer()
     /** Add a first string */ 
     i = write_cbuffer(cb,(void*)"TEST1");
     _assert(i == 1);
+    print_cb(cb);
     /** Add a second string */ 
     i = write_cbuffer(cb,(void*)"TEST2");
     _assert(i == 2);
+    print_cb(cb);
     /** Add a third string */ 
     i = write_cbuffer(cb,(void*)"TEST3");
     _assert(i == 3);
+    print_cb(cb);
     /** Add a fourth string but losing first one */ 
     i = write_cbuffer(cb,(void*)"TEST4");
     _assert(i == 3);
+    print_cb(cb);
+
     /** Read second string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST2")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    inc_readcbuffer(cb);
+    _inc_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST3")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    inc_readcbuffer(cb);
+    _inc_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST4")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    inc_readcbuffer(cb);
+    _inc_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST4")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    dec_readcbuffer(cb);
+    _dec_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST3")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    dec_readcbuffer(cb);
+    _dec_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST2")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    dec_readcbuffer(cb);
+    _dec_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST2")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    inc_readcbuffer(cb);
+    _inc_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST3")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    inc_readcbuffer(cb);
+    _inc_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST4")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    inc_readcbuffer(cb);
+    _inc_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST4")==0);
+    print_cb(cb);
     /** Increment Read pointer */
-    dec_readcbuffer(cb);
+    _dec_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
     _assert(i == 3);
     _assert(strcmp(c,"TEST3")==0);
+    print_cb(cb);
     /** Add a fifth string but losing second one */ 
     i = write_cbuffer(cb,(void*)"TEST5");
     _assert(i == 3);
+    print_cb(cb);
+
+    /** Increment Read pointer */
+    _dec_readcbuffer(cb);
     /** Read third string added */
     c = (char *) read_cbuffer(cb);
-    printf("%d %s\n",i ,c);
     _assert(i == 3);
-    _assert(strcmp(c,"TEST5")==0);
+    _assert(strcmp(c,"TEST4")==0);
+    print_cb(cb);
 
     return 0;
 }
